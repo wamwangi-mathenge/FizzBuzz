2603bb324203dac4b0669c8acfae4d543d940723
Brian <wamwangimathenge@gmail.com>
2023-07-21 12:40:26 +0300

    Write pseudocode for the program
    This commit details the steps that need to be taken to write the
    program

---
+++ script.js
@@ -0,0 +1,18 @@
+// Write a short program that prints each number from 1 to 20 on a new line
+// For each multiple of 3, print "Fizz" instead of the new number
+// For each multiple of 5, print "Buzz" instead of the new number
+// For numbers which are multiples of 3 and 5, print "FizzBuzz" instead of the number
+
+
+// Write a FOR LOOP that iterates over 1 to 20
+// Set the counter to 1
+// Break when the counter reaches 20
+// Increment counter by 1
+    // IF number MOD 15 == 0
+        // print "FizzBuzz"
+    // ELSE IF number MOD 3 == 0
+        // print "Fizz"
+    // ELSE IF number MOD 5 == 0
+        // print "Buzz"
+    // ELSE
+        // print i
