dc9ad1a5e6873790e9f3b63e777d50c555d39536
Brian <wamwangimathenge@gmail.com>
2023-07-21 12:44:53 +0300

    Write FizzBuzz Program
    This commit writes a short program that prints each number from 1 to 20
    For each multiple of 3, print "Fizz"
    For each multiple of 5, print "Buzz"
    For each multiple of 3 and 5, print "FizzBuzz"

--- script.js
+++ script.js
@@ -16,3 +16,16 @@
         // print "Buzz"
     // ELSE
         // print i
+
+
+for (i = 1; i <= 20; i++) {
+    if(i % 15 == 0) {
+        console.log("FizzBuzz")
+    } else if(i % 3 == 0) {
+        console.log("Fizz")
+    } else if(i % 5 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
